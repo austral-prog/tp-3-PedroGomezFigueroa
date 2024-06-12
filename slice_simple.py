0007c5d24eb520823cf38d515239b621ae2749a0
PedroGomezFigueroa <162721991+PedroGomezFigueroa@users.noreply.github.com>
2024-06-12 18:05:04 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,5 @@
 def slice_simple():
     texto = "Awesome"
-    print (f"texto = {texto}")
-
-    print(texto[0:3].lower())
+    print(texto[:3].lower())
     print(texto[2:5].lower())
     print(texto.lower())
