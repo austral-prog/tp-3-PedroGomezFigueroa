2541bac116b521157bd115abcf4992fbf80602f1
PedroGomezFigueroa <162721991+PedroGomezFigueroa@users.noreply.github.com>
2024-06-12 18:07:19 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -10,11 +10,11 @@ def check_vowels():
     I = ("I")
     O = ("O")
     U = ("U")
-    print ("contiene a:",(a in nombre) or (A in nombre))
-    print ("contiene e:",(e in nombre) or (E in nombre))
-    print ("contiene i:",(i in nombre) or (I in nombre))
-    print ("contiene o:",(o in nombre) or (O in nombre))
-    print ("contiene u:",(u in nombre) or (U in nombre))
+    print ("Contiene a:",(a in nombre) or (A in nombre))
+    print ("Contiene e:",(e in nombre) or (E in nombre))
+    print ("Contiene i:",(i in nombre) or (I in nombre))
+    print ("Contiene o:",(o in nombre) or (O in nombre))
+    print ("Contiene u:",(u in nombre) or (U in nombre))
     # Código a implementar utilizando input.
 
 
